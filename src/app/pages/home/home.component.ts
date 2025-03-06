814fb962a5d24890ce3f84a78ea03977016f547b
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-06 12:16:25 +0100

    feat/partnerSection

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { PartnerComponent } from './partner/partner.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [HeroComponent, PartnerComponent],
   templateUrl: './home.component.html',
   styles: ""
 })
---
+++ src/app/pages/home/partner/partner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-partner',
+  imports: [],
+  templateUrl: './partner.component.html',
+  styles: ""
+})
+export class PartnerComponent {
+
+}
