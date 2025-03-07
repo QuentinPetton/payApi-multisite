fa2e7f606a5b2d27ed83815e2e352ffeadf9a8c1
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-07 11:38:06 +0100

    feat/homePage featuresSection

---
+++ src/app/pages/home/features/features.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-features',
+  imports: [],
+  templateUrl: './features.component.html',
+  styles: ""
+})
+export class FeaturesComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { PartnerComponent } from './partner/partner.component';
+import { FeaturesComponent } from './features/features.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [HeroComponent, PartnerComponent],
+  imports: [HeroComponent, PartnerComponent, FeaturesComponent],
   templateUrl: './home.component.html',
   styles: ""
 })
