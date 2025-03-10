360a954e1edf3f3a6b94667a148d891d6bbba663
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-10 14:57:11 +0100

    feat/menu:state open/close

--- src/app/shared/burger-menu/burger-menu.component.ts
+++ src/app/shared/burger-menu/burger-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styles: ""
 })
 export class BurgerMenuComponent {
+isActive = signal(false);
 
+handleBurgerClick() {
+  this.isActive.set(!this.isActive());
+  console.log('Burger menu clicked');
+}
 }
