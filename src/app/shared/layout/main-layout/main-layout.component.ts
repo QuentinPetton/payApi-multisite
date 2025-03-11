0f6f55828af1cdccce62b6401a22d6207952e046
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-11 16:13:41 +0100

    feat/footer: add footerComponent & style

--- src/app/shared/layout/main-layout/main-layout.component.ts
+++ src/app/shared/layout/main-layout/main-layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BurgerMenuComponent } from '../../burger-menu/burger-menu.component';
 import { HeaderComponent } from '../../header/header.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-main-layout',
-  imports: [BurgerMenuComponent, RouterOutlet, HeaderComponent],
+  imports: [BurgerMenuComponent, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styles: ""
 })
