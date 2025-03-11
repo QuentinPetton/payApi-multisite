757ba293b303b1598715ab1f1c0b222dd0fea4d9
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-11 15:04:19 +0100

    feat/menu: add header component

--- src/app/shared/layout/main-layout/main-layout.component.ts
+++ src/app/shared/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BurgerMenuComponent } from '../../burger-menu/burger-menu.component';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   standalone: true,
   selector: 'app-main-layout',
-  imports: [BurgerMenuComponent, RouterOutlet],
+  imports: [BurgerMenuComponent, RouterOutlet, HeaderComponent],
   templateUrl: './main-layout.component.html',
   styles: ""
 })
