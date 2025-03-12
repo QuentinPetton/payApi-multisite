42a21d0ca699eee4aedd2300285fb3d72871bf08
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-12 12:22:22 +0100

    feat/princingPage: add pricingCard component to modularize Pricing section

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PricingCardComponent } from '../shared/pricing-card/pricing-card.component';
 
 @Component({
   standalone: true,
   selector: 'app-pricing',
-  imports: [],
+  imports: [PricingCardComponent],
   templateUrl: './pricing.component.html',
   styles: ""
 })
