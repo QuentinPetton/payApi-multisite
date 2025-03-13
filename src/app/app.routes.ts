27b9eb83963d964c30e31dd52840882de5fffed6
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-13 15:23:16 +0100

    feat/aboutPage: create aboutComponent & route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import type { Routes } from '@angular/router';
 import { MainLayoutComponent } from './shared/layout/main-layout/main-layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PricingComponent } from './pricing/pricing.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'pricing', component: PricingComponent },
+      { path: 'about', component: AboutComponent },
     ]
   }
 ];
